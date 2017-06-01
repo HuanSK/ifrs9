336ab2289f1dc9699a1e52bcd44c10d55ef94811
SubhashiniGunda <Subhashini_Gunda@westpac.co.nz>
2017-06-01 12:24:18 +1200

    new branch test

--- notebooks/Shared/IFRS/testGIT.scala
+++ notebooks/Shared/IFRS/testGIT.scala
@@ -1,1 +1,23 @@
 // Databricks notebook source
+//CIF thing
+
+// COMMAND ----------
+
+//adding a new line 
+i=0
+
+// COMMAND ----------
+
+//change from cif?
+
+// COMMAND ----------
+
+bal = x=x
+
+// COMMAND ----------
+
+//latest test
+
+// COMMAND ----------
+
+//PR test
